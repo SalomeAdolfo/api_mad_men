9cbcdf726dcc64deb2ed1d600b13be9aebc57c12
Salome-Hernandez-Adolfo-Angel <64882685+Salome-Hernandez-Adolfo-Angel@users.noreply.github.com>
2022-03-27 16:23:39 -0600

    First commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('primer_apellido',50);
+            $table->string('segundo_apellido',40);
+            $table->string('sexo',['femenino', 'masculino', 'prefiero no decirlo']);
+            $table->date('fecha_nacimiento');
+            $table->string('numero_telefono',10);
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
